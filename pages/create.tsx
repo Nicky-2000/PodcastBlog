a6bacc7d4f42d2957773c81fa7a6486f6b7cab49
Nicholas Khorasani <nckhoras@uwaterloo.ca>
2023-05-11 15:36:53 -0400

    Add log

--- pages/create.tsx
+++ pages/create.tsx
@@ -12,6 +12,7 @@ const Draft: React.FC = () => {
 
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
+    console.log("SUBMITTING DATA")
     try {
       const body = { title, content };
       await fetch('/api/post', {
