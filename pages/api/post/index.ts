ee16871167d0c9ed3c41f865ce173108ceeea59d
Nicholas Khorasani <nckhoras@uwaterloo.ca>
2023-05-11 15:43:28 -0400

    Pass email through

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -7,7 +7,7 @@ import prisma from '../../../lib/prisma';
 // Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req, res) {
-  const { title, content } = req.body;
+  const { title, content, email } = req.body;
   console.log("HERE????")
   console.log(title)
   const session = await getSession({ req });
@@ -15,7 +15,7 @@ export default async function handle(req, res) {
     data: {
       title: title,
       content: content,
-      author: { connect: { email: session?.user?.email } },
+      author: { connect: { email: email } },
     },
   });
   res.json(result);
